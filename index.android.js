c9fda5ac04487deba971f61d053b4203cf381cca
PranavPinarayi <pranavt@qburst.com>
2017-03-28 18:54:49 +0530

    react-navigation

--- index.android.js
+++ index.android.js
@@ -5,74 +5,31 @@
  */
 
  import React, { Component } from 'react';
- import Header from './components/Header';
- import Autocomplete from './components/Autocomplete';
+ import BusSearch from './src/pages/BusSearch';
  import { Provider } from 'react-redux';
  import {
    AppRegistry,
    StyleSheet,
-   Text,
-   View
  } from 'react-native';
+  import { StackNavigator } from 'react-navigation';
 
  import { createStore } from 'redux'
- import todoApp from './redux/modules/test/reducer'
- let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+ import busReducer from './src/redux/modules/test/reducer'
+
+ let store = createStore(busReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default class Bus extends Component {
-  filteredList = ['aa', 'bb', 'cc', 'dd', 'aab', 'aac', 'bbd'];
-  onSelect = (value) => {
-    console.log(`${value} selected`);
-  };
-
-  onChange = (value) => {
-    console.log('value', value);
-    console.log('this.filteredList', this.filteredList);
-    this.setState({filteredList: this.filteredList.filter((listItem) => listItem.includes(value))});
-  };
-
-  state = {
-    filteredList: [],
-  };
-
   render() {
-    console.log(store.val);
     return (
       <Provider key="provider" store={store}>
-      <View style={styles.container}>
-        <Autocomplete
-         data={this.state.filteredList}
-         hintText={'From'}
-         onChange={this.onChange}
-         onSelect={this.onSelect}
-        />
-      </View>
+        <BusSearch />
       </Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    padding: 30,
-    backgroundColor: '#E0FFFF',
-    flexDirection: 'column'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  button: {
-    flex:1,
-  }
+
 });
 
 AppRegistry.registerComponent('Bus', () => Bus);
